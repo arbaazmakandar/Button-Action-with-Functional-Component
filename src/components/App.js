80303e59383c4afc68b3d2515c434592461efb3b
arbaazmakandar <30730654+arbaazmakandar@users.noreply.github.com>
2021-08-31 16:57:49 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [state, setState] = React.useState("");
+  const handleClick = () => {
+    const text =
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    setState(text);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button onClick={handleClick} id="click">
+        Click me
+      </button>
+      {state ? <p id="para ">{state}</p> : ""}
     </div>
   );
 }
-
 
 export default App;
